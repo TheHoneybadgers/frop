186539871cb79e4c87356647a9413a61d488e7cb
Taylor Peet <repeet13@gmail.com>
2013-10-04 12:55:57 -0400

    Basic badges api backend created (just lists)

--- fropWeb/badges.php
+++ fropWeb/badges.php
@@ -1,1 +1,10 @@
-<?php ?>
+<?php 
+	include 'db_helper.php';
+	
+	function listBadges() {
+		$dbQuery = sprintf("SELECT `BADGE_ID`, `NAME`, `GOAL`, `GOAL_UNITS`, `DESCRIPTION`, `PRE_REQ`, `CUSTOM_PIC_URL`, `ADDED_DATE` FROM `BADGES`");
+		$result = getDBResultsArray($dbQuery);
+		header("Content-type: application/json");
+		echo json_encode($result);
+	}
+?>
