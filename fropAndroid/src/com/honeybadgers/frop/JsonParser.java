95447b7117ea27202a3ba672269c18f0f781bc00
Dlong88 <dlong88@gatech.edu>
2013-10-07 12:38:36 -0700

    EventList with the JSON parsing
    Currently using the apiary the app will parse the JSON and place each
    event into a list item.  The list Item is clickable that will bring you
    to a more detailed page for that event.

---
+++ fropAndroid/src/com/honeybadgers/frop/JsonParser.java
@@ -0,0 +1,48 @@
+package com.honeybadgers.frop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JsonParser {
+
+	public JsonParser() {
+
+	}
+
+	public JSONObject getJSONFromUrl(String url) {
+
+		try {//grab the JSON data from the URL and return the entities of it.
+			return new JSONObject(EntityUtils.toString(new DefaultHttpClient().execute(new HttpGet(url)).getEntity()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+}
