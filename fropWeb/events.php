71fd1dcef1b30f0e16dab39c50f04b124b885355
Taylor Peet <repeet13@gmail.com>
2013-10-17 14:44:58 -0400

    Modified badges and events to allow for basic POSTing

--- fropWeb/events.php
+++ fropWeb/events.php
@@ -2,6 +2,7 @@
 	include 'db_helper.php';
 	
 	function listEvents() {
+		
 		$dbQuery = sprintf("SELECT `EVENT_ID`, `DATE`, `ORG_ID`, `TITLE`, `FOURSQUARE`, `ADDRESS`, `START_TIME`, `END_TIME`, `APPROVED_DATE`, `SUMMARY`, `TYPE`, `SPECIAL_NOTES`, `ALCOHOL`, `CREATED_DATE`,`DATE_CHANGED` FROM `EVENTS`");
 		$result = getDBResultsArray($dbQuery);
 		header("Content-type: application/json");
@@ -22,15 +23,13 @@
 		echo json_encode($result);
 	}
 
-/*	
-	function addEvent($event) {
-		$dbQuery = sprintf("INSERT INTO events (event) VALUES ('%s')",
-			mysql_real_escape_string($events));
-	
-		$result = getDBResultInserted($dbQuery,'personId');
+	function addEvent($eventDate, $eventOrgId, $eventTitle, $eventFoursquare, $eventAddress, $eventStartTime, $eventEndTime, $eventSummary, $eventType, $eventSpecialNotes, $eventAlcohol) {
+		$dbQuery = sprintf("INSERT INTO EVENTS (`DATE`, `ORG_ID`, `TITLE`, `FOURSQUARE`, `ADDRESS`, `START_TIME`, `END_TIME`, `SUMMARY`, `TYPE`, `SPECIAL_NOTES`, `ALCOHOL`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", mysql_real_escape_string($eventDate), mysql_real_escape_string($eventOrgId), mysql_real_escape_string($eventTitle), mysql_real_escape_string($eventFoursquare), mysql_real_escape_string($eventAddress), mysql_real_escape_string($eventStartTime), mysql_real_escape_string($eventEndTime), mysql_real_escape_string($eventSummary), mysql_real_escape_string($eventType), mysql_real_escape_string($eventSpecialNotes), mysql_real_escape_string($eventAlcohol));
+		echo "Query " . $dbQuery . "</br>";
+		$result = getDBResultInserted($dbQuery,'EVENT_ID');
 		
 		header("Content-type: application/json");
-		echo json_encode($result);
+		echo json_encode($result) . "</br>";
 	}
 	
 	function updateEvent($id,$event) {
@@ -43,6 +42,5 @@
 		header("Content-type: application/json");
 		echo json_encode($result);
 	}
-*/
 
 ?>
