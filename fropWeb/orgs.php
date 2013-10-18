d9a367223f1fc2a847e54e977b648dfdb07f2eb9
Taylor Peet <repeet13@gmail.com>
2013-10-18 07:52:58 -0400

    added functionality for complete add (current date and time done for you

--- fropWeb/orgs.php
+++ fropWeb/orgs.php
@@ -15,9 +15,10 @@
 		header("Content-type: application/json");
 		echo json_encode($result);
 	}
-	
+  
 	function addOrg($orgLetters, $orgGovOrgId, $orgChapter, $orgNickname, $orgType, $orgFocus, $orgYearFounded, $orgYearChapterFounded, $orgBlurb, $orgFoursquare, $orgAddress, $orgHomepageUrl, $orgCustomPicUrl) {
-		$dbQuery = sprintf("INSERT INTO ORGS (`LETTERS`, `GOV_ORG_ID`, `CHAPTER`, `NICKNAME`, `TYPE`, `FOCUS`, `YEAR_FOUNDED`, `YEAR_CHAPTER_FOUNDED`, `BLURB`, `FOURSQUARE`, `ADDRESS`, `HOMEPAGE_URL`, `CUSTOM_PIC_URL`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s', '%s', '%s', '%s', '%s')", mysql_real_escape_string($orgLetters), mysql_real_escape_string($orgGovOrgId), mysql_real_escape_string($orgChapter), mysql_real_escape_string($orgNickname), mysql_real_escape_string($orgType), mysql_real_escape_string($orgFocus), mysql_real_escape_string($orgYearFounded), mysql_real_escape_string($orgYearChapterFounded), mysql_real_escape_string($orgBlurb), mysql_real_escape_string($orgFoursquare), mysql_real_escape_string($orgAddress), mysql_real_escape_string($orgHomepageUrl), mysql_real_escape_string($orgCustomPicUrl));
+		$dbQuery = sprintf("INSERT INTO ORGS (`LETTERS`, `GOV_ORG_ID`, `CHAPTER`, `NICKNAME`, `TYPE`, `FOCUS`, `YEAR_FOUNDED`, `YEAR_CHAPTER_FOUNDED`, `BLURB`, `FOURSQUARE`, `ADDRESS`, `HOMEPAGE_URL`, `CUSTOM_PIC_URL`, `PIC_APPROVED`) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s', '%s', '%s', '%s', '%s', '%s')", mysql_real_escape_string($orgLetters), mysql_real_escape_string($orgGovOrgId), mysql_real_escape_string($orgChapter), mysql_real_escape_string($orgNickname), mysql_real_escape_string($orgType), mysql_real_escape_string($orgFocus), mysql_real_escape_string($orgYearFounded), mysql_real_escape_string($orgYearChapterFounded), mysql_real_escape_string($orgBlurb), mysql_real_escape_string($orgFoursquare), mysql_real_escape_string($orgAddress), mysql_real_escape_string($orgHomepageUrl), mysql_real_escape_string($orgCustomPicUrl), "false");
+		
 		echo "Query " . $dbQuery . "</br>";
 		$result = getDBResultInserted($dbQuery,'ORG_ID');
 		
