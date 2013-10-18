21cd98894e4d6a7474f690b511372f6a346f72c9
Taylor Peet <repeet13@gmail.com>
2013-10-17 20:08:12 -0400

    POSTing to USERS should work now

--- fropWeb/users.php
+++ fropWeb/users.php
@@ -1,10 +1,19 @@
 <?php 
 	include 'db_helper.php';
 	
-	function listEvents() {
-		$dbQuery = sprintf("SELECT `EVENT_ID`, `DATE`, `ORG_ID`, `TITLE`, `FOURSQUARE`, `ADDRESS`, `START_TIME`, `END_TIME`, `APPROVED_DATE`, `SUMMARY`, `TYPE`, `SPECIAL_NOTES`, `ALCOHOL`, `CREATED_DATE`,`DATE_CHANGED` FROM `EVENTS`");
+	function getUser($id) {
+		$dbQuery = sprintf("SELECT * FROM `USERS` WHERE USER_ID = '%s' OR GTID = '%s'", mysql_real_escape_string($id), mysql_real_escape_string($id));
+		//echo "Query " . $dbQuery . "</br>";
 		$result = getDBResultsArray($dbQuery);
 		header("Content-type: application/json");
 		echo json_encode($result);
+	}
+
+	function addUser($userGITD, $userGovOrgId, $userPerms, $userOrgId) {
+		$dbQuery = sprintf("INSERT INTO `USERS` (`GTID`, `GOV_ORG_ID`, `PERMS`, `ORG_ID`) VALUES ('%s', '%s', '%s', '%s')", mysql_real_escape_string($userGITD), mysql_real_escape_string($userGovOrgId), mysql_real_escape_string($userPerms), mysql_real_escape_string($userOrgId));
+		//echo "Query " . $dbQuery . "</br>";
+		$result = getDBResultInserted($dbQuery,'USER_ID');
+		header("Content-type: application/json");
+		echo json_encode($result) . "</br>";
 	}
 ?>
